9fa2a2520f8a357828059881a17b52f675905272
Hannah Pawley <pawely10201@yahoo.com>
2020-09-22 17:23:46 -0400

    finished lab fixed sort

--- penguin.js
+++ penguin.js
@@ -1,5 +1,5 @@
 var penguinPromise = d3.json("classData.json");
-var setBanner = function(message)
+var setBanner = function (message)
 {
 	d3.select("#banner")
 	.text(message);
@@ -8,7 +8,7 @@ var setBanner = function(message)
 var drawTable = function(penguins)
 			 
 {
-	console.log("hi")
+	//console.log("hi")
 	var rows = d3.select("#penguinsTable tbody") 
 	.selectAll("tr")
 	.data(penguins)
@@ -16,22 +16,30 @@ var drawTable = function(penguins)
 	.append("tr")
 	
 	rows.append("td")
+	.classed("one",true)
 	.text(function(penguin)
 	{
 		return penguin.final[0].grade
 	})
 	
+	
+
+	
 	rows.append("td")
+	.classed("two",true)
 	.text(getGradeshomework)
 	
 	rows.append("td")
+	.classed("three",true)
 	.text(getGradestest)
 	
 	rows.append("td")
+	.classed("four",true)
 	.text(getGradesquiz)
 	
 	rows.append("td")
 	.append("img")
+	.classed("five",true)
 	.attr("src", function(penguins)
 	{
 		return ("imgs/"+penguins.picture)
@@ -68,10 +76,37 @@ var getGradesquiz = function(penguin)
 	{
 		return quiz.grade;
 	}
-	var getGrades2 = penguin.quiz.map(getGrade2)
+	var getGrades2 = penguin.quizes.map(getGrade2)
 	return d3.mean(getGrades2)
 }
 
+var sortfcn = function(penguins){
+	d3.select("#one")
+	.on("click",function (penguin)
+{
+	d3.selectAll("tbody tr")
+	.remove();
+	
+	penguins.sort(function (penguin1,penguin2)
+{
+	if (penguin1.final[0].grade == penguin2.final[0].grade)
+		{
+			return 0;
+		}
+	else if (penguin1.final[0].grade > penguin2.final[0].grade)
+		{
+			return 1;
+		}
+	else 
+		{
+			return -1;
+		}
+
+ })
+		drawTable(penguins)
+})
+
+}
 
 
 
@@ -81,6 +116,8 @@ var successFcn = function(penguins)
 	console.log("penguins",penguins)
 	setBanner("Here are the penguins!")
 	drawTable(penguins)
+	sortfcn(penguins)
+	
 }
 var failureFcn = function(error)
 {
